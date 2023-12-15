3a0002e6d6d3a0c6d8f3e315c1228b198deccfdb
SeongSeokwon <pam9411@naver.com>
2023-12-15 17:19:17 +0900

    로그인 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Router from "./components/Router";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Loader from "components/layouts/Loader";
 
 function App() {
   const auth = getAuth(app);
 
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
 
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,20 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { app } from "firebaseApp";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+
+type LoginDataType = {
+  email: string;
+  password: string;
+};
+
 const LoginForm = () => {
+  const [error, setError] = useState<string>("");
+  const [loginData, setLoginData] = useState<LoginDataType>({
+    email: "",
+    password: "",
+  });
+
+  const onSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth(app);
+      const {email, password} = loginData;
+      await signInWithEmailAndPassword(auth, email, password);
+      toast.success('로그인에 성공했습니다');
+    } catch (err: any) {
+      toast.error(err?.code);
+      console.log(err);
+    }
+  }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      const validRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (!value?.match(validRegex)) {
+        setError("이메일 형식이 올바르지 않습니다.");
+      } else {
+        setError("");
+      }
+    }
+    if (name === "password") {
+      if (value?.length < 8) {
+        setError("비밀번호는 8자리 이상으로 입력해주세요");
+      } else {
+        setError("");
+      }
+    }
+
+    setLoginData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
-    <form className="form form--lg">
+    <form onSubmit={onSubmit} className="form form--lg">
       <h1 className="form__title">로그인</h1>
       <div className="form__block">
         <label htmlFor="email">이메일</label>
-        <input type="email" name="email" id="email" required />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={onChange}
+        />
       </div>
       <div className="form__block">
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" required />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={onChange}
+        />
       </div>
+
+      {error && error.length > 0 && (
+        <div className="form__block">
+          <div className="form_-error">{error}</div>
+        </div>
+      )}
+
       <div className="form__block">
-        계정이 없으신가요? <Link to='/signup' className="form__link">회원가입</Link>
+        계정이 없으신가요?
+        <Link to="/signup" className="form__link">
+          회원가입
+        </Link>
       </div>
       <div className="form__block">
         <input type="submit" value="로그인" className="form__btn--submit" />
